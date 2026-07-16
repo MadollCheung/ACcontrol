11b5d9269957ed6da714efe576f8c17a5828a325
MadollCheung <175587985@qq.com>
2026-07-16 10:28:19 +0800

    Refactor AcState from data class to regular class

--- app/src/main/java/com/example/accontrol/model/AcState.kt
+++ app/src/main/java/com/example/accontrol/model/AcState.kt
@@ -1,10 +1,10 @@
 package com.example.accontrol.model
-
-data class AcState(
-    var isPowerOn: Boolean = false,
-    var temperature: Int = 24,
-    var fanSpeed: Int = 2,
-    var blowMode: Int = 0,
-    var isInnerCirculation: Boolean = true,
-    var isCoolingOn: Boolean = true
-)
+class AcState {
+    var driverTemp: Int = 20
+    var passengerTemp: Int = 20
+    var isAutoTemp: Boolean = false
+    var isPowerOn: Boolean = false
+    var fanSpeed: Int = 0
+    var blowMode: Int = 0
+    var isInnerCirculation: Boolean = false
+}
