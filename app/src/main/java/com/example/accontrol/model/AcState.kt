9ddd4c53acd09ca1749582a59de340a4a04cb05d
MadollCheung <175587985@qq.com>
2026-07-17 10:15:11 +0800

    Update AcState.kt

--- app/src/main/java/com/example/accontrol/model/AcState.kt
+++ app/src/main/java/com/example/accontrol/model/AcState.kt
@@ -1,10 +1,12 @@
 package com.example.accontrol.model
-class AcState {
-    var driverTemp: Int = 20
-    var passengerTemp: Int = 20
-    var isAutoTemp: Boolean = false
-    var isPowerOn: Boolean = false
-    var fanSpeed: Int = 0
-    var blowMode: Int = 0
-    var isInnerCirculation: Boolean = false
-}
+
+data class AcState(
+    var isPowerOn: Boolean = false,
+    var driverTemp: Int = 25,
+    var passengerTemp: Int = 25,
+    var isAutoTemp: Boolean = false,
+    var fanSpeed: Int = 2,
+    var blowMode: Int = 0,
+    var isInnerCirculation: Boolean = true,
+    var isDualZone: Boolean = false
+)
